3892ce19eded36f8a888bae108a2cf0407b1d189
Mizuta <mizutaeric@gmail.com>
2023-01-24 23:00:41 -0300

    change fetch to apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,17 @@
-import {useState, useEffect} from "react";
+import react from "react";
+import { graphql } from '@apollo/client/react/hoc';
+import {GET_POKEMON} from './endpoint/queries/queries'
 
-function App() {
-
-  const [pokemons, setPokemons] = useState({})
-  console.log(pokemons)
-  
-  const consumirAPI = async (graphqlEndpoint: any, query:any) => {
-    const response = await fetch(graphqlEndpoint, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ query })
-    });
-    
-    return response.json();
-  }
-  
-const GRAPHQL_ENDPOINT = 'https://beta.pokeapi.co/graphql/v1beta'
-
-const gen1pokemon = `query samplePokeAPIquery {
-  pokemon_v2_pokemonspecies(where: {pokemon_v2_generation: {name: {_eq: "generation-i"}}}) {
-    id
-    name
-    
-    pokemon_v2_pokemoncolor {
-      name
-    }
-    
-    pokemon_v2_pokemons {      
-      pokemon_v2_pokemontypes {
-        pokemon_v2_type {
-          name
-        }
-      }
-      
-      pokemon_v2_pokemonsprites {
-        sprites
-      }
-    }
-  }
-}`
-
-
-useEffect(() => {
-  consumirAPI(GRAPHQL_ENDPOINT, gen1pokemon).then((data) => {setPokemons(data)});
-}, []);
-
+function App(props:any) {
+  console.log(props.data)
+  const {pokemon_v2_pokemonspecies} = props.data
+  console.log(pokemon_v2_pokemonspecies)
 
   return (
     <>aaa</>
   )
 }
 
-export default App
+// export default App
+
+export default graphql(GET_POKEMON)(App);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App'
 import './index.css'
 
+const client = new ApolloClient({
+  uri: 'https://beta.pokeapi.co/graphql/v1beta',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 )
